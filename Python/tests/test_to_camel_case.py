76971f8b48033a8a8216d6b2e365ac1039b0f8a3
Davíd Roggér <davidrogger.a@gmail.com>
2022-12-11 08:45:32 -0300

    test: added new inputs

--- Python/tests/test_to_camel_case.py
+++ Python/tests/test_to_camel_case.py
@@ -7,6 +7,10 @@ def test_convert_to_camel_case():
         ("The_Stealth_Warrior", "TheStealthWarrior"),
         ("", ""),
         ("A-B-C", "ABC"),
+        ("A-Cat_was_Hungray", "ACatWasHungry"),
+        ("a_Pippi-was-pippi", "aPippiWasPippi"),
+        ("The_pippi-was_Savage", "ThePippiWasSavage"),
+        ("The_PippiWasPippi", "ThePippiWasPippi"),
     ]
 
     for input, expect in script_expects:
