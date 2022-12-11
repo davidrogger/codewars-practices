a333f6ad5b2e618c0227fb4408ab6f2ae44cbee4
Davíd Roggér <davidrogger.a@gmail.com>
2022-12-11 08:41:42 -0300

    feat: python conver to camel case

--- Python/practices/to_camel_case.py
+++ Python/practices/to_camel_case.py
@@ -1,2 +1,19 @@
+def discouver_separetor(string):
+    if "-" in string:
+        return "-"
+    elif "_" in string:
+        return "_"
+
+
 def to_camel_case(string: str) -> str:
-    return
+    separetor = discouver_separetor(string)
+
+    if separetor:
+        splited_phrase = string.split(separetor)
+
+        for index in range(1, len(splited_phrase)):
+            splited_phrase[index] = splited_phrase[index].capitalize()
+
+        return "".join(splited_phrase)
+
+    return string
