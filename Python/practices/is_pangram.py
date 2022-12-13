d39d3d67eb9c04e16870db757a5fe9e416951990
Davíd Roggér <davidrogger.a@gmail.com>
2022-12-13 09:16:40 -0300

    feat: added condition to be case insensitive

--- Python/practices/is_pangram.py
+++ Python/practices/is_pangram.py
@@ -1,2 +1,8 @@
 def is_pangram(phrase: str) -> bool:
-    pass
+    need_letters_to_be_pangram = set("abcdefghijklmnopqrstuvwxyz")
+
+    for letter in need_letters_to_be_pangram:
+        if letter not in phrase.lower():
+            return False
+
+    return True
