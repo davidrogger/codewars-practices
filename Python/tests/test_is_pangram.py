c9bbe187725deb23b029d31d49966308f3ce2e5c
Davíd Roggér <davidrogger.a@gmail.com>
2022-12-13 09:16:06 -0300

    test: added more tests

--- Python/tests/test_is_pangram.py
+++ Python/tests/test_is_pangram.py
@@ -2,9 +2,15 @@ from practices.is_pangram import is_pangram
 
 
 def test_is_pagram_when_is_true():
-    expect = True
-    phrase = "The quick, brown fox jumps over the lazy dog!"
-    assert is_pangram(phrase) == expect
+    expect_scripts = [
+        ("Cwm fjord bank glyphs vext quiz", True),
+        ("The quick, brown fox jumps over the lazy dog!", True),
+        ("Pack my box with five dozen liquor jugs.", True),
+        ("How quickly daft jumping zebras vex.", True),
+        ("ABCD45EFGH,IJK,LMNOPQR56STUVW3XYZ", True),
+    ]
+    for phrase, expect in expect_scripts:
+        assert is_pangram(phrase) == expect
 
 
 def test_is_pagram_when_is_false():
