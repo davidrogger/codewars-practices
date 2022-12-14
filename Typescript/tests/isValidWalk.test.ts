8206b75f6fa1178ee0323c07413350b8ea44923c
Davíd Roggér <davidrogger.a@gmail.com>
2022-12-14 09:57:09 -0300

    test: added more tests

--- Typescript/tests/isValidWalk.test.ts
+++ Typescript/tests/isValidWalk.test.ts
@@ -7,25 +7,54 @@ describe("Testing is valid walk", () => {
       const directions = [
         ['n', 's', 'n', 's', 'n', 's', 'n', 's', 'n', 's'],
         ['n', 'n', 'n', 'n', 'n', 's', 's', 's', 's', 's'],
-        ['n', 'n', 'n', 'w', 'w', 'w', 's', 's', 's', 'e', 'e', 'e']
+        ['w', 'w', 'w', 'w', 'w', 'e', 'e', 'e', 'e', 'e'],
+        ['w', 'e', 'w', 'e', 'w', 'e', 'w', 'e', 'w', 'e'],
       ]
       directions.forEach((direction: string[]) => {
         expect(isValidWalk(direction)).to.be.eq(true)
       })
-  
+
     })
   })
 
   describe("When go to a walk and not come back", () => {
     it("Should return false", () => {
       const directions = [
-        ['n'],
-        ['s', 's', 's', 's', 's'],
-        ['n', 'n', 'n', 'w', 'w', 'w', 's', 's', 's', 'e', 'e']
+        ['n', 'n', 'n', 's', 'n', 's', 'n', 's', 'n', 's'],
+        ['s', 's', 's', 's', 's', 's', 'n', 's', 'n', 's'],
+        ['n', 'n', 'n', 'w', 'w', 'w', 's', 's', 's', 'e'],
       ]
       directions.forEach((direction: string[]) => {
         expect(isValidWalk(direction)).to.be.eq(false)
       })
     })
   })
+
+  describe("When the walk is less than 10 steps", () => {
+    it("Should return False", () => {
+      const directions = [
+        ['n', 's'],
+        ['w', 'e', 'w', 'e'],
+        ['n', 'n', 'n', 's', 's', 's'],
+      ]
+      directions.forEach((direction: string[]) => {
+        expect(isValidWalk(direction)).to.be.eq(false)
+      })
+    })
+  })
+
+  describe("When the walk is bigger than 10 steps", () => {
+    it("Should return False", () => {
+      const directions = [
+        ['n', 's', 'n', 's', 'n', 's', 'n', 's', 'n', 's', 'n', 's'],
+        ['n', 'n', 'n', 'n', 'n', 'n', 's', 's', 's', 's', 's', 's'],
+        ['w', 'w', 'w', 'w', 'w', 'w', 'e', 'e', 'e', 'e', 'e'],
+        ['w', 'e', 'w', 'e', 'w', 'e', 'w', 'e', 'w', 'e', 'w', 'e'],
+      ]
+      directions.forEach((direction: string[]) => {
+        expect(isValidWalk(direction)).to.be.eq(false)
+      })
+    })
+  })
+
 })
