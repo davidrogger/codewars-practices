f48b7cb14085f5e2ac5c3ba0faab3b9dc1b5de87
Davíd Roggér <davidrogger.a@gmail.com>
2022-12-14 09:35:42 -0300

    feat: walking and coming back

--- Typescript/practices/isValidWalk.ts
+++ Typescript/practices/isValidWalk.ts
@@ -1,5 +1,20 @@
 function isValidWalk(directions: string[]): boolean {
-  return false;
+  const positions = {
+    x: 0,
+    y: 0,
+  };
+
+  directions.forEach((coordinate: string): void => {
+    const axis = coordinate === 'n' || coordinate === 's' ? 'y' : 'x';
+    const moviment = coordinate === 'n' || coordinate === 'e' ? 'positive' : 'negative';
+    if (moviment === 'positive') {
+      positions[axis] += 1;
+    } else {
+      positions[axis] -= 1;
+    }
+  });
+
+  return (positions.x + positions.y) === 0;
 }
 
 export default isValidWalk;
